7a5930585f968ac3084ad2ee827efd8f0b6a817b
MonforteAdrian <adryanmonforte94@gmail.com>
2022-08-16 16:55:28 +0200

    Things

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -92,7 +92,9 @@ return packer.startup(function(use)
   use 'famiu/bufdelete.nvim'
   use 'norcalli/nvim-colorizer.lua'
   use 'lewis6991/impatient.nvim'
-  use 'windwp/nvim-autopairs' 
+  use { "windwp/nvim-autopairs",
+    config = function() require("nvim-autopairs").setup {} end
+  }
   Config = {
     -- Move to lua dir so impatient.nvim can cache it
     compile_path = vim.fn.stdpath('config')..'/lua/packer_compiled.lua'
