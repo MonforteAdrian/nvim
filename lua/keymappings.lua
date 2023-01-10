ffed3fa818d5b2924b9d9abda85736cdccff1fb4
Adrian Monforte <adrian.monforte@egym.com>
2023-01-10 09:14:57 +0100

    Removed esc keymap as i have it in the keyboard

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -7,9 +7,6 @@ local function map(mode, lhs, rhs, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
--- Nvim
-map('i', 'ii', '<Esc>')                                     -- ii insert to espcape
-
 map('n', '<leader>h', '<C-w>h')
 map('n', '<leader>j', '<C-w>j')
 map('n', '<leader>k', '<C-w>k')
