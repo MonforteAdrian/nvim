6b689424faa5d258475e262aaa6f0181ec823963
Adrian Monforte <adrian.monforte@egym.com>
2023-01-23 11:23:00 +0100

    changes lsp now it works!

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -1,6 +1,3 @@
-require'lspconfig'.clangd.setup{}
-require("mason").setup()
-require("mason-lspconfig").setup()
+require "config.lsp.mason"
 require("config.lsp.handlers").setup()
 require "config.lsp.null-ls"
-require "config.lsp.lspsaga"
